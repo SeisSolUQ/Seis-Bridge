3bad174ddfc6cd3cc6c572a6949f91a20cd2b627
Sebastian Wolf <sebwolf@login2.frontera.tacc.utexas.edu>
2024-02-07 10:51:42 -0600

    Allow 2D input

--- client/client.py
+++ client/client.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import multiprocessing as mp
 import time
 import umbridge
@@ -19,13 +20,18 @@ if __name__ == "__main__":
                         print("Server not available")
                         time.sleep(10)
 
-        def eval_um_model(traction_middle):
+        def eval_um_model(traction_left, traction_middle):
                 config = {"order": 6}
-                return model([[78e6, traction_middle * 1e6, 62e6]], config)
+                return model([[traction_left * 1e6, traction_middle * 1e6, 62e6]], config)
 
         start_time = time.time() 
-        with mp.Pool(10) as p:
-                result = p.map(eval_um_model, [81.2, 81.3, 81.4, 81.5, 81.6, 81.7, 81.8, 81.9, 82.0, 82.1])
+        tractions_middle = [81.2, 81.3, 81.4, 81.5, 81.6, 81.7, 81.8, 81.9, 82.0, 82.1]
+        tractions_left = [78.0, 79.0, 80.0, 81.0, 82.0, 83.0]
+        arguments = [a for a in itertools.product(tractions_left, tractions_middle)]
+        number_of_models = len(arguments)
+        print(f"Evaluate {number_of_models} in parallel")
+        with mp.Pool(number_of_models) as p:
+                result = p.starmap(eval_um_model, arguments)
                 print(result)
         end_time = time.time()
         print(end_time - start_time)
